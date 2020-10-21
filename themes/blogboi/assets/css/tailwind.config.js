59120b1bbed139f3ee40ef1ab8522f2b325a9717
Bobby Chen <chenbobby168@gmail.com>
2020-10-21 01:58:07 -0400

    Designs header and bodies.

--- themes/blogboi/assets/css/tailwind.config.js
+++ themes/blogboi/assets/css/tailwind.config.js
@@ -4,8 +4,18 @@ module.exports = {
       'sans': 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
       'nunito': 'Nunito',
     },
-    extend: {}
+    extend: {
+      colors: {
+        blue: {
+          default: '#4a9ec1',
+          dark: '#2a627a',
+          darker: '#163441',
+        },
+      },
+    },
   },
-  variants: {},
-  plugins: []
+  variants: {
+    backgroundColor: ['hover'],
+  },
+  plugins: [],
 }
