d2109702f7346e395bfc91c98a2ab191b7a93209
Bobby Chen <chenbobby168@gmail.com>
2020-10-20 20:03:07 -0400

    Adds custom fonts and logo.

--- themes/blogboi/assets/css/tailwind.config.js
+++ themes/blogboi/assets/css/tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   theme: {
+    fontFamily: {
+      'sans': 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+      'nunito': 'Nunito',
+    },
     extend: {}
   },
   variants: {},
